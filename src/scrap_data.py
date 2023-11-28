2517f345492180d4da5b1da48c1ca7d33b241fee
Ramon Coronado <ramoncormo8@gmail.com>
2023-11-28 17:51:43 +0100

    data_with_wikipedia

---
+++ src/scrap_data.py
@@ -0,0 +1,11 @@
+from scrapers import wikipedia_scraper
+import pandas as pd
+
+if __name__ == "__main__":
+    path = '../data/data.csv'
+    df = pd.read_csv(path, sep=',')
+    df['wikipedia_v1'] = df['Company name'].apply(wikipedia_scraper.wikipedia_v1)
+    df['wikipedia_v2'] = df['Company name'].apply(wikipedia_scraper.wikipedia_v2)
+    df['wikipedia_v3'] = df['Company name'].apply(wikipedia_scraper.wikipedia_v3)
+    
+    df.to_csv(path, index=False, sep=',')
