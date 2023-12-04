aab591b32cd8fa0f92202b32bb4c0fe9f06683cf
uhernando002 <100877449+uhernando002@users.noreply.github.com>
2023-12-04 22:15:43 +0100

    conexion con API_CHATGPT

--- src/scrap_data.py
+++ src/scrap_data.py
@@ -4,6 +4,6 @@ import pandas as pd
 if __name__ == "__main__":
     csv_file = "/home/unai/datasets/POC Description of operations - Sheet3.csv"
     df = pd.read_csv(csv_file)
-    df["WEB"] = df["URL"].apply(scraper_web.extract_all_data)
+    df["WEB_2"] = df.head(15)["URL"].apply(scraper_web.extract_all_text)
 
     df.to_csv(csv_file, index=False, sep=",")
