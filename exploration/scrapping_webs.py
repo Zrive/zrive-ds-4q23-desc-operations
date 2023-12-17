736d631aabc23a9c1a00abf06943342855adc347
pamalo9 <pamalo2002@gmail.com>
2023-12-17 18:29:12 +0100

    changes in exploration

--- exploration/scrapping_webs.py
+++ exploration/scrapping_webs.py
@@ -1,24 +1,25 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import logging
 
-# To display all the text of the column
+PATH = "data/POC Description of operations - Sheet3.csv"
+PATH_RESULT =  "data/POC Description of operations - Sheet3_updated.csv"
+
 pd.set_option("display.max_colwidth", None)
 
-data = pd.read_csv(
-    "/home/pamalo9/zrive-ds-4q23-desc-operations/src/data/POC Description of operations - Sheet3.csv"
-)
+data = pd.read_csv(PATH)
 
 
 def realizar_scraping(url):
-    # Add http:// to avoid errors
+  
     if not url.startswith("http://") and not url.startswith("https://"):
         url = "https://" + url
     try:
         response = requests.get(url)
         soup = BeautifulSoup(response.text, "html.parser")
 
-        # Take all the text  of the web
+        
         web_text = soup.get_text(separator=" ", strip=True)
 
         if web_text:
@@ -29,26 +30,25 @@ def realizar_scraping(url):
             return None
 
     except Exception as e:
-        print(f"Scrapping error {url}: {e}")
+        logging.error(f"Scrapping error {url}: {e}")
         return None
 
 
-# Add new column to dataset
+
 data["description"] = None
 
-# Go through each row
+
 for index, row in data.iterrows():
     company_name = row["Company_NAME"]
     url_company = row["URL"]
 
-    # Call the function
+    
     description = realizar_scraping(url_company)
 
-    # Update column
+   
     data.at[index, "description"] = description
 
 
-data.to_csv(
-    "/home/pamalo9/zrive-ds-4q23-desc-operations/src/data/POC Description of operations - Sheet3_updated.csv",
+data.to_csv(PATH_RESULT,
     index=False,
 )
