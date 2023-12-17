3a6db77e705f1de4cb5596d33a918901a6c48328
pamalo9 <pamalo2002@gmail.com>
2023-12-17 17:45:55 +0100

    Changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 from src.data_processing import data_logic
 import numpy as np
 
@@ -19,19 +20,20 @@ from src.Hugging_face_summarizers.Hugging_face_sum1 import get_summary
 =======
 from src.Hugging_face_summarizer.Hugging_face_sum1 import get_summary
 >>>>>>> b50d2f2 (Take the final summarizer in src and change path in main)
+=======
+from src.data_summarizer.bart_large_text_summarizer import get_summary
+>>>>>>> fb3aed8 (Changes)
 import pandas as pd
 
 
+PATH_CSV = "data/POC Description of operations - Sheet3_final.csv"
+FINAL_PATH_CSV = "data/company_summary.csv"
+
+
 def main():
-    PATH_CSV = "src/data/POC Description of operations - Sheet3_final.csv"
     df_data = pd.read_csv(PATH_CSV)
 
-    # Call function
     final_df = get_summary(df_data)
-
-    FINAL_PATH_CSV = "src/data/company_summary.csv"
-
-    # Final CSV src/data/FINAL.csv
     final_df.to_csv(FINAL_PATH_CSV, index=False)
 
 
