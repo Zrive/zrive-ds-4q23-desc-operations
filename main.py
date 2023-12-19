5017a1262af3f4ecc16235e57965959ccb89e97d
uhernando002 <100877449+uhernando002@users.noreply.github.com>
2023-12-19 22:17:18 +0100

    come changes in logic

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ from src.data_processing import data_logic
 ORIGINAL_FILE_PATH = "data/Original_POC Description of operations - Sheet3.csv"
 MODIFIED_FILE_PATH = "data/Mod_POC Description of operations - Sheet3.csv"
 
+SUMMARIZER_SELECTOR = 1
 
 """ 
 If SUMMARIZER_SELECTOR is 0, Code will execute api_chatgpt. 
 If SUMMARIZER_SELECTOR is 1, Code will execute hugging_face model
 For other cases, ERROR
+STATUS: 
+- 0 -> WEB SCARPING
+- 1 -> SEARCH_ENIGNE BING
+- 2 -> SEARCH_ENIGNE DDG
+- 3 -> SEARCH_ENIGNE GOOGLE
+- 4 -> SEARCH_ENIGNE WIKIPEDIA
+- 5 -> ERROR
 """
-SUMMARIZER_SELECTOR = 1
 
 def main():
   
@@ -18,11 +25,9 @@ def main():
     
     data_sample = data.sample(n=10, random_state=1)
 
-    data_sample["Description"] = data_sample.apply(data_logic.get_description, axis=1, summarizer_selector = SUMMARIZER_SELECTOR)
+    data_sample[['Description', 'Status']] = data_sample.apply(data_logic.get_description, axis=1, summarizer_selector = SUMMARIZER_SELECTOR)
     
     data_sample.to_csv(MODIFIED_FILE_PATH, index=False, sep=",")
    
-    
-
 if __name__ == "__main__":
    main()
--- src/data_processing/data_logic.py
+++ src/data_processing/data_logic.py
@@ -16,10 +16,13 @@ def data_load(file_path: str) -> pd.DataFrame:
     df = pd.read_csv(file_path)
     df["Description"] = np.nan
     df["Description"] = df["Description"].astype(object)
+    df["Status"] = np.nan
+    df["Status"] = df["Status"].astype(object)
     return df
 
 
-def get_description(row: pd.Series, summarizer_selector: int) -> str:
+def get_description(row: pd.Series, summarizer_selector: int) -> tuple[str, int]:
+    status = 0
     print(row["Company_NAME"])
     html = scraper_web.request_html(row["URL"])
     if str(html).lower().find("error!:") != -1:
@@ -29,13 +32,17 @@ def get_description(row: pd.Series, summarizer_selector: int) -> str:
             google_scraper.google,
             wikipedia_scraper.wikipedia,
         ]
-        for function in search_engine_funcitons:
+        for i, function in enumerate(search_engine_funcitons, start=1):
             try:
+                status = status + 1
                 text = function(row["Company_NAME"])
-                break
+                if text and isinstance(text, str):
+                    break
+                if i > 3:
+                    status = status + 1
+                    return np.nan, status
             except Exception as e:
                 print(f"Error in {function.__name__}: {e}")
-        return np.nan
     else:
         text = scraper_web.clean_html_text(html)
         if len(text) >= 4096:
@@ -43,13 +50,12 @@ def get_description(row: pd.Series, summarizer_selector: int) -> str:
 
     if summarizer_selector == 0:
         description = api_chatgpt.chatgpt_call(text, row["Company_NAME"])
+        time.sleep(30)
 
     elif summarizer_selector == 1:
         description = bart_large_text_summarizer.get_summary(text)
 
     else:
         raise ValueError("The value of  'SUMMARIZER_SELECTOR' must be 0 or 1")
-
-    time.sleep(30)
 
     return description
